64c551a7238b4e04fdea1becac9b2f8502f9d5c0
= <normoslem256@gmail.com>
2024-12-15 11:35:56 +0300

    refactor(simple_tokenizer.py): Simplify merging logic and enhance clarity
    - Replaced in-place modification of 'raws' with a new list to improve code readability and avoid side effects.
    - Streamlined the merging process by ensuring 'new_raws' is updated only after processing all top pairs, enhancing the overall logic of the tokenizer.
    - Improved the structure of the merging loop for better clarity and maintainability.

--- tokenizer/simple_tokenizer.py
+++ tokenizer/simple_tokenizer.py
@@ -82,11 +82,11 @@ class SimpleTokenizer:
                 break
 
             top_pairs = get_topk_pair(pairs, k)
+            new_raws = []
             for pair in top_pairs:
                 token_id = len(self.vocab)
                 self.vocab[token_id] = self.decode(pair)
 
-                new_raws = []
                 for raw in raws:
                     merged = []
                     continue_merge = False
@@ -101,7 +101,7 @@ class SimpleTokenizer:
                             merged.append(item[0])
                     if merged:
                         new_raws.append(merged)
-                raws = new_raws
+            raws = new_raws
 
 
         for i, token in enumerate(self.special_tokens, start=1):
